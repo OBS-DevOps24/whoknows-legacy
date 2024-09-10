2cddbf8f637ee5b31417e1c50a842714f36cc8de
Benjamin-Harris1 <beha1503@gmail.com>
2024-09-10 16:09:57 +0200

    added search + login route with empty html.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,18 @@ import Layout from "./Layout";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
 
     <Layout>
       <Routes>
+        <Route path="/" element={<Search />} />
         <Route path="/about" element={<About />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Layout>
   );
---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <p>Login form goes here</p>
+        </div>
+    )
+}
+
+export default Login;

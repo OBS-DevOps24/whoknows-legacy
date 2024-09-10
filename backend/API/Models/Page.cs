4bab75f1fed8e5b281f32d780b1dad9489ef9f7e
osman-butt <butt.osman@gmail.com>
2024-09-11 01:26:57 +0200

    Added Page Model

---
+++ backend/API/Models/Page.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    [Table("Pages")]
+    public class Page
+    {
+        [Key]
+        [Required]
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Url]
+        [Column("url")]
+        public string Url { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Column("language")]
+        public string Language { get; set; }
+
+        [Column("last_updated")]
+        public DateTime LastUpdated { get; set; }
+        
+        [Required]
+        [Column("content")]
+        public string Content { get; set; }
+    }
+}

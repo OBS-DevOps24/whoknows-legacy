4ba82502492e5462f73504fca4cf8bb7a1abad26
osman-butt <butt.osman@gmail.com>
2024-09-10 13:49:32 +0200

    Added react router dom usage example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,12 @@ import "./index.css";
 
 function App() {
   return (
+    // React router example
+    // <Layout>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //   </Routes>
+    // </Layout>
     <>
       <div className="w-full h-screen">
         <div className="flex items-center justify-center w-full h-full">

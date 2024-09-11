8cde0f1a9eeab9743c37ee245e3c78a68c7f881b
Benjamin-Harris1 <beha1503@gmail.com>
2024-09-11 20:36:28 +0200

    Added repository + interface with basic methods

---
+++ backend/API/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByUsernameAsync(string username);
+    }
+}
---
+++ backend/API/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+    }
+}

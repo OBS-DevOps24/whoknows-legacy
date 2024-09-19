7c4cff7d634213c468014ddfd05a95d39a918e9d
osman-butt <butt.osman@gmail.com>
2024-09-19 02:47:09 +0200

    Removed unused imports

--- frontend/src/pages/search/SearchPage.tsx
+++ frontend/src/pages/search/SearchPage.tsx
@@ -1,7 +1,5 @@
-import { useSearchParams } from "react-router-dom";
 import Search from "./Search";
 import SearchResults from "./SearchResults";
-import { PageType } from "../../interfaces/types";
 
 export default function SearchPage() {
   return (

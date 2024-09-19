390654cc1f5868d181afe8d09ba3330d5921b06f
osman-butt <butt.osman@gmail.com>
2024-09-19 10:46:56 +0200

    Changed env

--- frontend/Settings.ts
+++ frontend/Settings.ts
@@ -1,15 +1,8 @@
-const isProduction = import.meta.env.MODE === "prod";
+const isDevelopment = import.meta.env.MODE === "dev";
+console.log("isDevelopment", isDevelopment);
 
-let URL = isProduction
-  ? import.meta.env.VITE_PROD_API_BASE_URL
-  : import.meta.env.VITE_DEV_API_BASE_URL;
+const URL = isDevelopment ? import.meta.env.VITE_DEV_API_BASE_URL : "/api";
 
-const MODE = isProduction ? "prod" : "dev";
-console.info(MODE + " API URL: " + URL);
-// console.info("ENV", import.meta.env);
-
-if (!URL) {
-  URL = "/api";
-}
+console.log("URL", URL);
 
 export const API_URL = URL;

7fbdad432112ad75facfe9c3c6745e9a6d2d61ba
Benjamin-Harris1 <beha1503@gmail.com>
2024-09-19 11:54:01 +0200

    added migrations

---
+++ backend/API/Migrations/20240919094729_AddUserTable.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pages",
+                table: "Pages");
+
+            migrationBuilder.RenameTable(
+                name: "Pages",
+                newName: "pages");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_pages",
+                table: "pages",
+                column: "title");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    username = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_pages",
+                table: "pages");
+
+            migrationBuilder.RenameTable(
+                name: "pages",
+                newName: "Pages");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pages",
+                table: "Pages",
+                column: "title");
+        }
+    }
+}
--- backend/API/Models/User.cs
+++ backend/API/Models/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Models
 {
-    [Table("Users")]
+    [Table("users")]
     public class User
     {
         [Key]

87b8ef576289f420805b88f90c5903ca067f26f4
osman-butt <butt.osman@gmail.com>
2024-09-19 02:40:53 +0200

    Created SearchResults component

--- frontend/src/pages/search/SearchPage.tsx
+++ frontend/src/pages/search/SearchPage.tsx
@@ -1,21 +1,13 @@
 import { useSearchParams } from "react-router-dom";
 import Search from "./Search";
+import SearchResults from "./SearchResults";
+import { PageType } from "../../interfaces/types";
 
 export default function SearchPage() {
-  const [searchParams] = useSearchParams();
-
   return (
     <div>
-      <h1>Search</h1>
       <Search />
-      <div className="my-2">
-        {searchParams.get("q") && (
-          <p className="text-[14px]">
-            Showing results for{" "}
-            <span className="font-bold">{searchParams.get("q")}</span>
-          </p>
-        )}
-      </div>
+      <SearchResults />
     </div>
   );
 }

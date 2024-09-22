fda9385bc1f2ee891e344c21c55505095394fbcd
osman-butt <butt.osman@gmail.com>
2024-09-22 16:11:35 +0200

    Weather model

---
+++ backend/API/Models/Dtos/WeatherDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Models.Dtos
+{
+    public class WeatherDTO
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Time { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string TemperatureUnit { get; set; }
+        public double TemperatureValue { get; set; }
+        public string WindSpeedUnit { get; set; }
+        public double WindSpeedValue { get; set; }
+    }
+}

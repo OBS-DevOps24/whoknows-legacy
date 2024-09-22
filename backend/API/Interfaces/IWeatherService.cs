a4c97c1a9413b47ddd9b28c8af846b6991c91cc2
osman-butt <butt.osman@gmail.com>
2024-09-22 16:11:48 +0200

    Weather service with controller

---
+++ backend/API/Interfaces/IWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using API.Models;
+using API.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Interfaces
+{
+    public interface IWeatherService
+    {
+        Task<WeatherDTO> GetWeatherData(double? latitude, double? longitude, string? city, string? country);
+    }
+}

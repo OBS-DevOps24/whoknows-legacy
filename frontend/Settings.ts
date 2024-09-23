be80f1aa372e235acc133124f105074501b4a44b
Benjamin-Harris1 <beha1503@gmail.com>
2024-09-23 10:16:05 +0200

    env + settings.tsx

--- frontend/Settings.ts
+++ frontend/Settings.ts
@@ -1,7 +1,7 @@
-const isDevelopment = import.meta.env.MODE === "dev";
+const isDevelopment = import.meta.env.DEV;
 console.log("isDevelopment", isDevelopment);
 
-const URL = isDevelopment ? import.meta.env.VITE_DEV_API_BASE_URL : "/api";
+const URL = isDevelopment ? import.meta.env.VITE_DEV_API_BASE_URL : import.meta.env.VITE_PROD_API_BASE_URL;
 
 console.log("URL", URL);
 

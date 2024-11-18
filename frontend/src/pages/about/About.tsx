a16eea8be4765a1999f394000f50ec62dbb489ff
Benjamin-Harris1 <beha1503@gmail.com>
2024-11-18 17:02:29 +0100

    about styling

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -1,13 +1,23 @@
 const About = () => {
     return (
-        <div>
-            <h1 className="text-2xl font-bold">Our mission</h1>
-            <p>We intend to build the world's best search engine!</p>
-            <h2 className="text-xl font-bold">Our team</h2>
-            <img className="h-40" src="https://placehold.co/600x400" alt="monk group our team" />
+        <div className="max-w-2xl mx-auto text-center space-y-8 py-10">
+            <div className="space-y-4">
+                <h1 className="text-3xl font-bold text-gray-800">Our mission</h1>
+                <p className="text-lg text-gray-600">
+                    We intend to build the world's best search engine!
+                </p>
+            </div>
+
+            <div className="space-y-4">
+                <h2 className="text-2xl font-bold text-gray-800">Our team</h2>
+                <p className="text-lg text-gray-600">
+                    <span className="font-medium">Osman</span> • 
+                    <span className="font-medium"> Benjamin</span> • 
+                    <span className="font-medium"> Sham</span>
+                </p>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default About;
-

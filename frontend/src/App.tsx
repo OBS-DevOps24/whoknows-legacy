728ec91d88abaffa2c1fc962e975e81bac0ce803
osman-butt <butt.osman@gmail.com>
2024-11-20 17:10:17 +0100

    Added route to change password and added SecurityBreachBanner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,22 +7,33 @@ import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
 import SearchPage from "./pages/search/SearchPage";
 import Weather from "./pages/weather/Weather";
+import ChangePassword from "./pages/changePassword/ChangePassword";
+import RequireAuth from "./pages/changePassword/RequireAuth";
+import SecurityBreachBanner from "./pages/changePassword/SecurityBreachBanner";
 
 function App() {
   return (
     <AuthProvider>
+      <SecurityBreachBanner />
       <Layout>
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="/about" element={<About />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/change-password"
+            element={
+              <RequireAuth>
+                <ChangePassword />
+              </RequireAuth>
+            }
+          />
           <Route path="/weather" element={<Weather />} />
-      </Routes>
+        </Routes>
       </Layout>
     </AuthProvider>
   );
 }
-
 
 export default App;

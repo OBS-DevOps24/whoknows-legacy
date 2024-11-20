9f6d6bfeb2857a0e7a2258fef7757a038d7d9dd1
Benjamin-Harris1 <beha1503@gmail.com>
2024-11-20 15:26:47 +0100

    added migrations for DB

---
+++ backend/API/Migrations/20241120142607_AddExpiredPassword.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpiredPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "expired_password",
+                table: "users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expired_password",
+                table: "users");
+        }
+    }
+}

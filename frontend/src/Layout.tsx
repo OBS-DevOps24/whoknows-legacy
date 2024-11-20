1434d4e6e3c0f590abd34849276f3e614ca2c104
osman-butt <butt.osman@gmail.com>
2024-11-20 16:15:07 +0100

    Added security breach banner

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -12,7 +12,19 @@ export default function Layout({ children }: LayoutProps) {
       <div className="w-full">
         <div className="mt-10 max-w-[1200px] mx-auto">{children}</div>
       </div>
-      <Footer />
+      <div>
+        <SecurityBreachBanner />
+        <Footer />
+      </div>
     </main>
+  );
+}
+
+function SecurityBreachBanner() {
+  return (
+    <div className="relative px-4 py-3 font-bold text-center text-red-700 bg-yellow-100 border border-yellow-400 rounded">
+      ⚠️ <strong>Security Alert:</strong> We have detected a potential security
+      breach. Please update your password immediately to secure your account.
+    </div>
   );
 }

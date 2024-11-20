960f29c77f43bdf21a888a07089346552ffc7e94
osman-butt <butt.osman@gmail.com>
2024-11-20 17:08:01 +0100

    Removed security breach banner

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -13,18 +14,8 @@ export default function Layout({ children }: LayoutProps) {
         <div className="mt-10 max-w-[1200px] mx-auto">{children}</div>
       </div>
       <div>
-        <SecurityBreachBanner />
         <Footer />
       </div>
     </main>
-  );
-}
-
-function SecurityBreachBanner() {
-  return (
-    <div className="relative px-4 py-3 font-bold text-center text-red-700 bg-yellow-100 border border-yellow-400 rounded">
-      ⚠️ <strong>Security Alert:</strong> We have detected a potential security
-      breach. Please update your password immediately to secure your account.
-    </div>
   );
 }
